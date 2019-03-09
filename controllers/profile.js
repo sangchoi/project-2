7a807d4c2ce7ff8823f4fee922c602ab79c2a398
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-03-09 11:45:11 -0800

    update works

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,34 +3,35 @@ var router = express.Router();
 const isLoggedIn = require('../middleware/isLoggedIn');
 var db = require('../models');
 
-// // GET EDIT PAGE WITH CURRENT USER AND PROFILE
-// router.get("/:id/edit", function(req, res) {
-//   db.user.findById(req.user.id).then(function(user) {
-//     user.getProfile().then(function(profile) {
-//       res.render("profile/edit", {user, profile})
-//     })
-//   })
-// })
+// GET EDIT PAGE WITH CURRENT USER AND PROFILE
+router.get("/:id/edit", function(req, res) {
+  db.user.findById(req.user.id).then(function(user) {
+    user.getProfile().then(function(profile) {
+      res.render("profile/edit", {user, profile})
+    })
+  })
+})
 
-// // POSTS THE EDITS TO THE PROFILE DATABASE
-// router.put("/:id", function(req, res) {
-//   db.user.findById(req.user.id).then(function(user) {
-//     user.getProfile().then(function(profile) {
-//       db.profile.update({
-//         birthYear: parseInt(req.body.birthYear),
-//         gender: req.body.gender,
-//         city: req.body.city,
-//         state: req.body.state,
-//         country: req.body.country,
-//         language: req.body.language
-//       }, {
-//         where: {id: parseInt(req.params.id)}
-//       }).then(function(data) {
-//         res.redirect('/profile')
-//       })
-//     })
-//   })
-// })
+// POSTS THE EDITS TO THE PROFILE DATABASE
+router.put("/:id", function(req, res) {
+  db.user.findById(req.user.id).then(function(user) {
+    user.getProfile().then(function(profile) {
+      db.profile.update({
+        birthYear: parseInt(req.body.birthYear),
+        gender: req.body.gender,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        language: req.body.language
+      }, {
+        where: {id: parseInt(req.params.id)}
+      }).then(function(data) {
+        res.redirect('/profile')
+      })
+    })
+  })
+})
+
 
 
 // Gets info from database
